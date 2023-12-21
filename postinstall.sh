4f71278f4a7eb3e3fdf84d6172e508ebf733dfa1
gesala <gesala@gmail.com>
2023-12-21 11:41:38 +0100

    Solucionados errores en los paths. Instalación de auto-cpufreq

--- postinstall.sh
+++ postinstall.sh
@@ -1,21 +1,20 @@
-wpa_passphrase ESSID PASSWORD>> /etc/wpa_supplicant/wpa_supplicant-wlp2s0b1.conf 
-cd /var/service/
-ln -s /etc/sv/wpa_supplicant .
-sv status wpa_supplicant
-dhcpcd 
-xbps-install -Su
+#wpa_passphrase ESSID PASSWORD>> /etc/wpa_supplicant/wpa_supplicant-wlp2s0b1.conf 
+#cd /var/service/
+#ln -s /etc/sv/wpa_supplicant .
+#sv status wpa_supplicant
+#dhcpcd 
 xbps-install xtools void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 xbps-install broadcom-wl-dkms
-xcheckrestart 
 xbps-install -Su
+xcheckrestart 
 xbps-install intel-ucode
-xbps-reconfigure --force linux$(uname -r)
+xbps-reconfigure --force linux6.5-$(uname -r)
 chsh -s /bin/bash root
 touch /etc/sv/sshd/down
-ln -s /et/sv/sshd /var/service/
+ln -s /etc/sv/sshd /var/service/
 xbps-install socklog-void
-ln -s /etc/sv/socklog-unix /var/services/
-ln -s /etc/sv/nanoklogd /var/services/
+ln -s /etc/sv/socklog-unix /var/service/
+ln -s /etc/sv/nanoklogd /var/service/
 xbps-install cronie
 ln -s /etc/sv/crond /var/service
 cat >/etc/cron.weekly/fstrim << EOF
@@ -25,10 +24,16 @@ EOF
 chmod u+x /etc/cron.weekly/fstrim
 xbps-install openntpd
 ln -s /etc/sv/ntpd /var/service/
-xbps-install tlp
-ln -s /etc/sv/tlp /var/services/
-xbps-install NetworkManager python3-dbus
-rm -f /var/service/wpa_supplicant
+#xbps-install tlp
+#ln -s /etc/sv/tlp /var/service/
+xbps-install thermald
+ln -s /etc/sv/thermald /var/service
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+cd ..
+sudo auto-cpufreq --install
+sudo rm -f /var/service/wpa_supplicant
+rm -f /var/service/dhcpcd
 ln -s /etc/sv/dbus /var/service
 ln -s /etc/sv/NetworkManager /var/service/
-nmcli dev wifi con "ESSID" password PASSWORD  name "XXX"
+#nmcli dev wifi con "ESSID" password PASSWORD  name "XXX"
